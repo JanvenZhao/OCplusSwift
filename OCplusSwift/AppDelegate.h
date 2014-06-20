02c80d33a0bfe324c257b65e6f3f7292f2402992
JanvenZhao <jianwen2010@gmail.com>
2014-06-20 10:15:08 +0800

    Initial Commit

---
+++ OCplusSwift/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OCplusSwift
+//
+//  Created by jian.zhao on 14-6-20.
+//  Copyright (c) 2014年 jian.zhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
